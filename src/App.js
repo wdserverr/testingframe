5ac0056bed2b04f384ca309adb4dabf2b589e443
wdserverr <42107311+wdserverr@users.noreply.github.com>
2022-05-09 00:29:22 +0700

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  const handleClick = () =>  {
+    alert('Free pizza!');
+}
+  return (
+    <div className="App">
+      <div>
+      <iframe src='https://paras.id/token/grimms.secretskelliessociety.near::422/422'>
+      </iframe>
+      </div>
+      <button
+      onClick={handleClick}
+      ></button>
+
+    </div>
+  );
+}
+
+export default App;
